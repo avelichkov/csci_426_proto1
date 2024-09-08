d0814bc40c93e5c435f2c3b998a17d4c44822aa0
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-09-08 16:59:22 -0700

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed;
+    public float reloadTime;
+    public float forceAmount;
+    public Transform tip;
+    public Target target;
+
+    public Rigidbody2D targetrb2d;
+    private float timer;
     private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = reloadTime;
+        targetrb2d = target.GetComponent<Rigidbody2D>();
         rb2d = GetComponent<Rigidbody2D> ();
     }
 
@@ -24,5 +33,25 @@ public class PlayerMovement : MonoBehaviour
         //rb2d.MovePosition((Vector2)transform.position + (horizontalInput * speed * Time.deltaTime * new Vector2(1,0)));
         
         transform.Translate(horizontalInput * new Vector2(1,0) * speed * Time.deltaTime);
+
+        // Check for shot
+        if (Input.GetKeyDown(KeyCode.Space))// && timer <= 0)
+        {
+            Debug.Log("shoot");
+            Shoot();
+            timer = reloadTime;
+        }
+        timer -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        //get angle
+        Vector2 playerToTarget = target.transform.position - tip.position;
+        float angle = Vector3.Angle(this.transform.position, playerToTarget) - 90;
+        targetrb2d.velocity = new Vector2(0f,0f);
+        target.Shot();
+        targetrb2d.AddForce(playerToTarget.normalized * forceAmount,ForceMode2D.Impulse);
+        Debug.Log("Angle: " + angle);
     }
 }
