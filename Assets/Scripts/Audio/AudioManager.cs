1fecaac7bb68248cf6db2198c447bed79e61b7d5
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-09-08 16:01:31 -0700

    audio system working

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake() {
+        if (instance == null) 
+        {
+            instance = this;
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        //Play("quack")
+        //or FindObjectOfType<AudioManager>.Play("Soundname");
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s !=null)
+        {
+            s.source.Play();
+        } 
+        else
+        {
+            Debug.LogWarning("Could not find sound: " + name);
+        }
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s !=null)
+        {
+            s.source.Stop();
+        } 
+        else
+        {
+            Debug.LogWarning("Could not find sound: " + name);
+        }
+    }
+}
