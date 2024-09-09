0fd2ee72862f4eea73439ad12215a0051387e8ef
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-09-08 18:07:22 -0700

    working on death

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class Target : MonoBehaviour
 {
     public PlayerMovement player;
+    public GameObject ground;
     public ParticleSystem collisionParticles;
     public Rigidbody2D rg2d;
     public bool once = true;
@@ -14,7 +16,10 @@ public class Target : MonoBehaviour
     // Start is called before the first frame update
     public void Update()
     {
-        once = true;
+        if (IsColliding())
+        {
+            StartCoroutine(Explode());
+        }
         
     }
     public void Shot()
@@ -26,14 +31,30 @@ public class Target : MonoBehaviour
         var dur = collisionParticles.main.duration;
 
         emission.enabled = true;
-        collisionParticles.Play();
-        once = false;      
+        collisionParticles.Play();     
+    }
+
+    public IEnumerator Explode()
+    {
+        //Do particle effects
+        yield return new WaitForSeconds(0f);
+        RestartGame();
     }
     private bool IsColliding()
     {
         Collider2D colA = GetComponent<Collider2D>();
-        Collider2D colB = player.GetComponent<Collider2D>();
+        Collider2D colB = player.GetComponent<PolygonCollider2D>();
+        Collider2D colC = ground.GetComponent<Collider2D>();
         Debug.Log(colA.IsTouching(colB));
         return colA.IsTouching(colB);
+    }
+
+    public void RestartGame()
+    {
+        // Get the current scene
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Reload the current scene by its name or build index
+        SceneManager.LoadScene(currentScene.name);
     }
 }
