dbca5b143a434d462ac50d48ac3778c0d915b7af
Towhid Aziz <towhidaz@usc.edu>
2024-09-08 18:46:38 -0700

    particle change

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public bool start = false;
+    public float duration = 1f;
+
+    void Update()
+    {
+        if (start) {
+            start = false;
+            StartCoroutine(Shaking());
+        }
+    }
+
+    IEnumerator Shaking() {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration) {
+            elapsedTime += Time.deltaTime;
+            transform.position = startPosition + (Random.insideUnitSphere * 0.2f);
+            yield return null;
+        }
+
+        transform.position = startPosition;
+    }
+}
