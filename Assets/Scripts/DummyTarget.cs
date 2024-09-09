28c40c88fbdc410dc0f38f655ffa5301299f04c0
Towhid Aziz <towhidaz@usc.edu>
2024-09-08 20:21:08 -0700

    Update DummyTarget.cs

--- Assets/Scripts/DummyTarget.cs
+++ Assets/Scripts/DummyTarget.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class DummyTarget : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     public void Initialize(Vector2 pos)
     {
         transform.position = pos;
+        var emission = GetComponent<ParticleSystem>().emission;
+        emission.enabled = true;
+        GetComponent<ParticleSystem>().Play();
+
         FindAnyObjectByType<AudioManager>().Play("explosion");
     }
 }
