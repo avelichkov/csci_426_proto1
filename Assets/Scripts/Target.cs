21047c3c7a0b97e7eaf08ba0f05817917cce1f82
Towhid Aziz <towhidaz@usc.edu>
2024-09-08 17:29:33 -0700

    Update Target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public ParticleSystem collisionParticles;
+    public Rigidbody2D rg2d;
+    public bool once = true;
+    
     // Start is called before the first frame update
+    public void Update()
+    {
+        Shot();
+        once = true;
+        
+    }
     public void Shot()
     {
+        Vector3 CurrentVelocity = rg2d.velocity;
+
         Debug.Log("I was shot");
+        if (CurrentVelocity.y > 0 && once){
+            var emission = collisionParticles.emission;
+            var dur = collisionParticles.main.duration;
+
+            emission.enabled = true;
+            collisionParticles.Play();
+            once = false;      
+        }
     }
+
 }
